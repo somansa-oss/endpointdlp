5b700495c42bc77bd7ba14b9399396d599b318b6
minku1024 <minku1024@gmail.com>
2020-10-27 11:43:11 +0900

    update the header file PIESF.h

--- main/PIKern/PIESF.h
+++ main/PIKern/PIESF.h
@@ -1,9 +1,10 @@
 #ifndef _PIESF_h_
 #define _PIESF_h_
 
+#ifdef LINUX
+
 #include "PIObject.h"
 
-#ifdef LINUX
 #include <stdio.h>
 #include <signal.h>
 #include <string.h>
@@ -32,7 +33,13 @@ typedef struct _monitored {
 
 #endif
 
+#ifdef LINUX
 class CPIESF : public CPIObject {
+#else
+class CPIESF
+{
+#endif
+    
 public:
     CPIESF();
     ~CPIESF();
@@ -40,6 +47,7 @@ public:
 private:
     bool m_bIsContinue;
 
+#ifdef LINUX
 protected:
     pthread_t serverThread;
     pthread_attr_t serverThreadAttr;
@@ -51,26 +59,28 @@ protected:
     bool runningServerThread;
     bool runningClientThread;
 
-#ifdef LINUX
     int fanotify_fd;
     struct pollfd fds[FD_POLL_MAX];
 
 public:
     static monitored_t *monitors;
     static int n_monitors;
-#endif
 
 protected:
     bool startThreads(void);
     void waitThreads(void);
     bool isRunningServerThread(void);
     bool isRunningClientThread(void);
-
+#endif
+    
 public:
     bool isActive(void);
+
+#ifdef LINUX
     virtual bool initialize(void);
     virtual bool finalize(void);
-
+#endif
+    
 public:
     static CPIESF& getInstance( void );
 
@@ -83,7 +93,10 @@ public:
 #endif
 
     void run(void);
+
+#ifdef LINUX
     int stop(void);
+#endif
 };
 
 #endif /* _PIESF_h_ */
